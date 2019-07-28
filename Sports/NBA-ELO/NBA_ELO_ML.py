a192c7783faa763e1498724f1acfbc85dfb76ba9
VCG-cphillips <47870744+VCG-cphillips@users.noreply.github.com>
2019-07-28 13:07:16 -0500

    Added Sports and Spotify Folders

---
+++ Sports/NBA-ELO/NBA_ELO_ML.py
@@ -0,0 +1,8 @@
+"""
+Read in CSV
+Play around with feeding most data into a model. 
+Use to try and predict the winner of playoff games. 
+Make regular season games and early (Pre-1990) playoff games the training set
+Modern Playoff games are the validation/testing set. 
+Goal is to predict the winner of Modern PLayoff games, and eventually pick a winner...somehow
+"""
