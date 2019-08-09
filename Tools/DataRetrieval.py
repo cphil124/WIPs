a467b1361ff388b1e44ba597fcc09c9e7850ad81
VCG-cphillips <47870744+VCG-cphillips@users.noreply.github.com>
2019-08-08 22:52:41 -0500

    Added main function and debugged many of primary methods
    Added handling for both zip files as well as tar.
    
    Next step is to incorporate automatic checking for archive file being retrieved and automatic unpacking into a same-named directory

--- Tools/DataRetrieval.py
+++ Tools/DataRetrieval.py
@@ -2,11 +2,13 @@
 import os
 import requests
 import tarfile
+import zipfile
 import sys
 
 DEFAULT_DIRECTORY = 'data/raw_data'
 
 FILETYPE_LIB = ['tgz', 'csv']
+ARCHIVE_LIB = ['tgz', 'bz2', 'tar', 'zip']
 
 
 class WebDataFetcher(object):
@@ -14,10 +16,15 @@ class WebDataFetcher(object):
     Class to fetch and handle the file-type processing to get the data ready for use
     """
 
-    def __init__(self):
-        self.data_dir = ''
-        self.data_file = ''
-        self.file_type = '' # To keep track of the type of file. Can make disparate format specific functions and then add aggregate functions to determine type and apply accordingly
+    def __init__(self, file):
+        if file:
+            self.data_file = os.path.abspath(file)
+            self.file_type = self.get_file_type_(self.data_file)
+            self.data_dir = os.path.dirname(self.data_file)
+        else:
+            self.data_file = ''
+            self.data_dir = ''
+            self.file_type = '' # To keep track of the type of file. Can make disparate format specific functions and then add aggregate functions to determine type and apply accordingly
 
     
     def fetch(self, data_url, save_path=DEFAULT_DIRECTORY, save_name='DATA'):
@@ -26,7 +33,7 @@ class WebDataFetcher(object):
         """
         # Runs get_file_type on the incoming data to confirm that it is a supported filetype.  If it is, then it will set the class attribute file_type for later reference. 
         # If it is not, then it it will exit the program.
-        self.file_type = self.get_file_type(file=data_url)
+        self.file_type = self.get_file_type_(file=data_url)
 
         # Create the save dir if it doesn't exist and add it to the instance attributes
         if not os.path.isdir(save_path):
@@ -34,13 +41,11 @@ class WebDataFetcher(object):
         
         self.data_dir = save_path
         
-        # get_file_type(
         # Code to check for type of download file and adapt file extension accordingly goes here
         file_path = os.path.join(save_path, (save_name + '.' + self.file_type))
         x = requests.get(data_url, allow_redirects=True)
         open(file_path, 'wb').write(x.content)
         self.data_file = os.path.abspath(file_path)
-        # self.
         print(self.data_file)
     
     def attrs(self):
@@ -50,7 +55,7 @@ class WebDataFetcher(object):
         for var in vars(self):
             print(var, ' ; type: ', type(vars(self)[var]), ' : ', vars(self)[var])
 
-    def get_file_type(self, file):
+    def get_file_type_(self, file):
         if file:
             data = file
         else:
@@ -64,27 +69,57 @@ class WebDataFetcher(object):
             print('This filetype handling not supported at present')
             sys.exit(1)
 
+    
+    def is_unzippable(self):
+        if self.file_type in ARCHIVE_LIB:
+            return True
+        else:
+            return False
+
+    def convert_github_url(self, gh_url, filename):
+        raw_url = gh_url.replace(old='github', new='raw.githubusercontent')
+        full_url = os.path.join(raw_url, filename)
+        return full_url
 
         
         
-    def extract_tgz_(self):
+    def extract_all_(self):
         """
         Right now only handles tgz files, should change so that it can dynamically extra
         """
-        # if self.file_type is tgz
-        tgz_path = self.data_file
+        # Setting of important variables
+        data = self.data_file
         save_path = self.data_dir
-        data_tgz = tarfile.open(tgz_path)
-        data_tgz.extractall(path=save_path)
-        data_tgz.close()
-        os.remove(tgz_path)
+
+        # Extract contents of file
+        if zipfile.is_zipfile(data):
+            f = zipfile.ZipFile(data)
+            f.extractall(path = data[:-4])
+            f.close()
+
+        elif tarfile.is_tarfile(data):
+            data_tgz = tarfile.open(name=data)
+            data_tgz.extractall(path=save_path)
+            data_tgz.close()
+        
+        os.remove(data)
 
 
-TEST_URL = 'https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.tgz'
+TEST_TGZ_URL = 'https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.tgz'
+TEST_CSV_URL = 'https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv'
+regular_url = 'https://github.com/ageron/handson-ml2/tree/master/datasets/housing'
 
 
+def main():
+    # fetcher = WebDataFetcher()
+    # fetcher.fetch(data_url = TEST_URL)
+    # print('got here')
+    # fetcher.attrs()
+    # print(fetcher.is_unzippable())
+    # fetcher.extract_tgz_()
+    fetcher = WebDataFetcher('data\\raw_data\\housing.csv')
+    fetcher.attrs
 
-fetcher = WebDataFetcher()
-# fetcher.fetch(data_url = TEST_URL)
-x = fetcher.get_file_type(file='target.tgz')
-print(x)
+
+if __name__ == "__main__":
+    main()
